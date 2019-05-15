3575877e88122bbd83d4f7d0be9e4c47069e7bae
naoki.tomita <mxcpy608@hotmail.com_>
2019-05-15 20:30:26 +0900

    Fix server port.

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ var server = http.createServer(function (request, response) {
   response.writeHead(404);
   response.end();
 });
-server.listen(80, function () {
-  console.log((new Date()) + ' Server is listening on port 80');
+server.listen(443, function () {
+  console.log((new Date()) + ' Server is listening on port 443');
 });
 
 wsServer = new WebSocketServer({
