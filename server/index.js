ec4a0b1fb1bdfc5db126fbe486c2cdf8aa2d6498
kojiro.ueda <mxcpy608@hotmail.com>
2020-03-24 19:07:24 +0900

    Refactoring.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 const WebSocketServer = require('websocket').server;
 const http = require('http');
 
-function incomingSlackWebhook(request) {
-
-}
-
 function parseQueryString(queryString) {
   return queryString
     .split("&").map(it => it.split("="))
@@ -20,62 +16,37 @@ async function parseBody(request) {
 }
 
 const server = http.createServer(async (request, response) => {
-  const [_, queryString] = request.url.split("?");
-  if (request.method.toLowerCase() === "get") {
-    response.end();
-    const query = parseQueryString(queryString);
-    console.log("incoming message: " + query.q);
-    incomingMessage(query.q);
-  } else if (request.method.toLowerCase() === "post") {
-    const body = await parseBody(request);
-    console.log(body);
-    if (body.type === "url_verification") {
-      response.statusCode = 200;
-      response.setHeader("content-type", "application/json");
-      response.end(JSON.stringify({ challenge: body.challenge }));
-      return;
+  try {
+    if (request.method.toLowerCase() === "get") {
+      const [_, queryString] = request.url.split("?");
+      const query = parseQueryString(queryString);
+      console.log("incoming message: " + query.q);
+      incomingMessage(query.q);
+    } else if (request.method.toLowerCase() === "post") {
+      const body = await parseBody(request);
+      if (body.type === "url_verification") {
+        response.statusCode = 200;
+        response.setHeader("content-type", "application/json");
+        response.end(JSON.stringify({ challenge: body.challenge }));
+        return;
+      }
+      if (body.event.type === "message") {
+        console.log("incoming slack message: " + body.event.text);
+        incomingMessage(body.event.text);
+      }
     }
-    response.end();
-    console.log(body);
-    incomingMessage(body.event.text);
+  } finally {
+    !response.writableEnded && response.end();
   }
 });
-server.listen(process.env.PORT, () => {
-  console.log(`${new Date()} Server is listening on port ${process.env.PORT}` );
-});
 
-wsServer = new WebSocketServer({
-  httpServer: server,
-  autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-  return true;
-}
-
+server.listen(process.env.PORT);
+wsServer = new WebSocketServer({ httpServer: server, autoAcceptConnections: false });
 const connections = [];
 wsServer.on('request', function (request) {
-  if (!originIsAllowed(request.origin)) {
-    request.reject();
-    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-    return;
-  }
-
   const connection = request.accept();
   connections.push(connection);
-  console.log((new Date()) + ' Connection accepted.');
-  connection.on('message', function (message) {
-    connections.forEach(connection => {
-      if (message.type === 'utf8') {
-        console.log('Received Message: ' + message.utf8Data);
-        connection.sendUTF(message.utf8Data);
-      } else if (message.type === 'binary') {
-        console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-        connection.sendBytes(message.binaryData);
-      }
-    });
-  });
-  connection.on('close', function (reasonCode, description) {
+  connection.on('close', () => {
     connections.splice(connections.findIndex(con => connection.remoteAddress === con.remoteAddress), 1);
     console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     console.log(`Connections: ${connections.length}`);
