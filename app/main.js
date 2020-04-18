55a03ee9ee35030d4e1ce1c73ec38a0a428a91ef
kojiro.ueda <mxcpy608@hotmail.com>
2020-04-18 10:42:13 +0900

    Bugfix It cant receive message after econnected.

--- app/main.js
+++ app/main.js
@@ -2,13 +2,29 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 const WebSocket = require("ws");
 const { join } = require("path");
 
-class MessageClient {
+class Observable {
   constructor() {
+    this.cbs = {};
+  }
+
+  on(type, cb) {
+    this.cbs[type] = [...(this.cbs[type] || []), cb];
+  }
+
+  emit(type, ...args) {
+    this.cbs[type].forEach(it => it(...args));
+  }
+}
+
+class MessageClient extends Observable {
+  constructor() {
+    super();
     console.log(`connecting to ${process.env.WS || "ws://ws-smile-server.herokuapp.com"}`);
     this.connect();
   }
 
   connect() {
+    this.ws?.removeAllListeners();
     this.ws = new WebSocket(process.env.WS || "ws://ws-smile-server.herokuapp.com");
     this.ws.once("open", () => console.log("connected"));
     this.ws.once("error", () => console.log("failed to connect."));
@@ -16,12 +32,10 @@ class MessageClient {
       console.log(`reconnecting...`);
       this.connect();
     }, 2000));
+    this.ws.on("message", (data) => this.emit("message", data))
   }
 
-  onOpen(cb) { this.ws.on("open", cb); }
-  onError(cb) { this.ws.on("error", cb); }
-  onMessage(cb) { this.ws.on("message", cb); }
-  onClose(cb) { this.ws.on("close", cb); }
+  onMessage(cb) { this.on("message", cb); }
   smile(msg) { this.ws.send(msg); }
 }
 
